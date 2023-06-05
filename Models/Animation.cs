4919b5d589931217d21bd252e04952df12df688b
Choollol <67297627+Choollol@users.noreply.github.com>
2023-06-05 09:44:37 -0700

    Add files via upload

---
+++ Models/Animation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slime.Models
+{
+    public class Animation
+    { 
+        public int currentFrame { get; set; }
+        public int frameCount { get; private set; }
+        public int frameWidth { get { return texture.Width / frameCount; } }
+        public int frameHeight { get { return texture.Height; } }
+        public float frameSpeed { get; set; }
+        public bool isLooping { get; set; }
+
+        public Texture2D texture { get; private set; }
+
+        public Animation(Texture2D newTexture, int newFrameCount)
+        {
+            texture = newTexture;
+            frameCount = newFrameCount;
+            isLooping = true;
+        }
+
+
+    }
+}
---
+++ Models/Input.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slime.Models
+{
+    public class Input
+    {
+        MouseState mState = Mouse.GetState();
+        public Keys moveUp { get; set; }
+        public Keys moveDown { get; set; }
+        public Keys moveLeft { get; set; }
+        public Keys moveRight { get; set; }
+        public Keys teleport { get; set; }
+        public Keys teleportHome { get; set; }
+        public Keys stopTime { get; set; }
+        public Keys startTime { get; set; }
+    }
+}
